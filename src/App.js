44e02072aaef1a619cdef2d2b1dfe9e47a38a280
lvasteli <lucijarocks@gmail.com>
2018-12-12 20:20:22 +0100

    Save colors and change background of component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//importing home component
+import { Home } from './components/Home';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorhex: []
+    }
+    this.LoadData=this.LoadData.bind(this);
+    this.LoadData();
+    this.LoadData();
+    
+  }
+  LoadData() {
+    fetch('http://www.colr.org/json/color/random')
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState(prevState => ({
+        colorhex: [...prevState.colorhex, "#"+data.colors[0].hex]
+      }));
+    });
+  }
+  // componentWillMount () {
+  //   //save 2 colors in state
+  //   this.LoadData();
+  //   this.LoadData();
+  // }
   render() {
+    let comp;
+    if(this.state.colorhex.length===2) {
+      comp = (<Home listofrandomcolors={this.state.colorhex}/>)
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          {comp}
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/components/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export class Home extends React.Component{
+    constructor(props) {
+        console.log(props);
+        super(props);
+        this.state= {
+            color: "",
+            black: "black",
+            randomCol: "",
+        };
+
+    }
+    getRandomColor() {
+        const items=this.props.listofrandomcolors;
+        let item = items[Math.floor(Math.random()*items.length)];
+        this.setState({
+            randomCol: item
+        })
+    }
+    componentWillMount() {
+        this.getRandomColor();
+    }
+    changeColor() {
+        if(this.state.color===""){
+            this.setState({
+                color: this.state.black
+            });
+        }
+        else if(this.state.color==="black"){
+            this.setState({
+                color: this.state.randomCol
+            });
+        } else {
+            this.setState({
+                color: this.state.black
+            });
+        }
+    }
+    render() {
+        let comp;
+        //console.log(this.props.black);
+        comp = (<h2> Text Of New Component </h2>)
+        return (
+            <div style={{background: this.state.color}}>
+                {comp}
+                <button onClick={()=>this.changeColor(this.props.listofrandomcolor,'black')}>Button of New Component</button>
+            </div>
+            
+        );
+    }
+};
